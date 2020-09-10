56f5ef0788f16d5cdb492f1196ed41293cc213ae
Steven Torrence <storrence88@gmail.com>
2020-09-09 20:41:50 -0500

    Build Recipes component index

--- app/javascript/routes/Index.js
+++ app/javascript/routes/Index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
+import Recipes from '../components/Recipes';
 
 console.log('In routes');
 
@@ -8,6 +9,7 @@ export default (
   <Router>
     <Switch>
       <Route path='/' exact component={Home} />
+      <Route path='/recipes' exact component={Recipes} />
     </Switch>
   </Router>
 );
